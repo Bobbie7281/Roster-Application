27e45d8045822461642123e2ecb48faa7f6579e9
Robert Ellul <rob.ellul@gmail.com>
2023-10-30 21:03:52 +0100

    Included new methods to be used by Jquery scripts

--- Roster Application/Controllers/ScheduleController.cs
+++ Roster Application/Controllers/ScheduleController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 using Roster_Application.Data;
 using Roster_Application.Models;
 using Roster_Application.Models.Models_Interface;
+using System.Text.RegularExpressions;
 
 namespace Roster_Application.Controllers
 {
@@ -20,16 +22,17 @@ namespace Roster_Application.Controllers
             _scheduleModel = scheduleModel;
             _categoryModel = categoryModel;
         }
+        [HttpPost]
         public IActionResult ScheduleOptions()
         {
             return View();
         }
-
         public IActionResult CreateNewSchedule()
         {
             _scheduleModel!.ScheduleName = "";
             return View(_scheduleModel);
         }
+
         [HttpPost, ActionName("Create Schedule")]
         public IActionResult CreateNewSchedule(ScheduleModel obj)
         {
@@ -41,14 +44,45 @@ namespace Roster_Application.Controllers
             return RedirectToAction("CreateNewSchedule", "Schedule");
         }
         [HttpPost]
-        public IActionResult CheckScheduleName(string inputValue) //This method is called by the Jquery script to check is the category name
-                                                                  //already exists in database without refreshing the whole page.
+        public IActionResult EditExistingSchedule()
+        {
+            var data = _db.Schedules.ToList();
+            return View(data);
+        }
+        public IActionResult EditSchedule(string selectedSchedule, string newSchName, string monHrs, string tueHrs, string wedHrs, string thurHrs, string friHrs, string satHrs, string sunHrs)
+        {
+            var obj = _db.Schedules.FirstOrDefault(x => x.ScheduleName == selectedSchedule);
+            bool isValid = false;
+
+            obj!.ScheduleName = newSchName;
+            obj.ScheduleMonTotHours = int.Parse(monHrs);
+            obj.ScheduleTueTotHours = int.Parse(tueHrs);
+            obj.ScheduleWedTotHours = int.Parse(wedHrs);
+            obj.ScheduleThurTotHours = int.Parse(thurHrs);
+            obj.ScheduleFriTotHours = int.Parse(friHrs);
+            obj.ScheduleSatTotHours = int.Parse(satHrs);
+            obj.ScheduleSunTotHours = int.Parse(sunHrs);
+
+            _db.Schedules.Update(obj);
+            _db.SaveChanges();
+
+            TempData["Successful"] = "Data Saved Successsfully!";
+            isValid = true;
+
+            return Json (new { isValid });
+        }
+        public IActionResult SCheckScheduleName(string inputValue) //This method is called by the Jquery script in the CreateNewSchedule view to check if the category name
+                                                                   //already exists in database without refreshing the whole page.
         {
             bool isValid;
+            string checkForWhitespace = @"\s";
 
-            var checkCategoryName = _db.Categories.FirstOrDefault(x => x.CategoryName == inputValue);//checks if the value of inputValue exists in the database and returns the name if it exists.
+            bool whitespaceDedected = Regex.IsMatch(inputValue, checkForWhitespace);
 
-            if (checkCategoryName == null)
+            var checkScheduleName = _db.Schedules.FirstOrDefault(x => x.ScheduleName == inputValue);//checks if the value of inputValue exists in the database and returns the name if it exists.
+
+
+            if (checkScheduleName == null && inputValue != null && !whitespaceDedected)
             {
                 isValid = true;
                 _scheduleModel!.ScheduleName = inputValue;
@@ -60,9 +94,83 @@ namespace Roster_Application.Controllers
 
             return Json(new { isValid });
         }
-        public IActionResult EditExistingSchedule()
+        [HttpPost]
+        public IActionResult SGetData(string scheduleName) //This method is called by the Jquery script in the EditExistingSchedule to get the data from the database according to the name of the selected schedule. 
         {
-            return View();
+            var data = _db.Schedules.FirstOrDefault(x => x.ScheduleName == scheduleName); //Search for the name of schedule and pass it to variable data.
+            return Json(data); //return data to the jQuery script.
+        }
+        [HttpPost]
+        public IActionResult SCheckDataPriorSaving(string selectedSchedule, string newSchName, string monHrs, string tueHrs, string wedHrs, string thurHrs, string friHrs, string satHrs, string sunHrs)
+        {
+
+            var obj = _db.Schedules.FirstOrDefault(x => x.ScheduleName == newSchName);//Check if the new schedule name already exists in the database.
+
+            string checkForWhitespace = @"\s"; //Regex expression which checks for a whitespace in a string,
+            string checkHoursFormat = "^[0-9]+$";//Regex expression which checks if a string contains only numbers without whitespaces.
+            bool correctNumberFormat;
+            bool whitespaceDetected = false;
+            string error = "Orange";
+            string noError = "Green";
+
+            List<string> errorsList = new List<string>();
+            List<string> hoursList = new List<string>() { monHrs, tueHrs, wedHrs, thurHrs, friHrs, satHrs, sunHrs };
+
+            int errors = 0;
+
+            if (newSchName == null)
+            {
+                errors++;
+                errorsList.Add(error);
+            }
+            else
+            {
+                whitespaceDetected = Regex.IsMatch(newSchName, checkForWhitespace);
+            }
+
+
+            if (whitespaceDetected)
+            {
+                errors++;
+                errorsList.Add(error);
+            }
+            else if (obj != null && newSchName!=selectedSchedule)
+            {
+                errors++;
+                errorsList.Add(error);
+            }
+            else if(obj!=null && obj!.ScheduleName==selectedSchedule)
+            {
+                errorsList.Add(noError);
+            }
+            else if (!whitespaceDetected && obj == null && newSchName != null)
+            {
+                errorsList.Add(noError);
+            }
+
+            foreach (string item in hoursList)
+            {
+                if (item != null)
+                {
+                    correctNumberFormat = Regex.IsMatch(item, checkHoursFormat);
+                    if (correctNumberFormat)
+                    {
+                        errorsList.Add(noError);
+                    }
+                    else
+                    {
+                        errors++;
+                        errorsList.Add(error);
+                    }
+                }
+                else
+                {
+                    errors++;
+                    errorsList.Add(error);
+                }
+            }
+            errorsList.Add(errors.ToString());
+            return Json(errorsList);
         }
     }
 }
