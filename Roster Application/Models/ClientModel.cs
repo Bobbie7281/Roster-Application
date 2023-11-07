8993ae3b5404485181d43f0861a53354495b8cc5
Robert Ellul <rob.ellul@gmail.com>
2023-11-07 22:25:59 +0100

    Included data Annotation for the Display Names of the view

--- Roster Application/Models/ClientModel.cs
+++ Roster Application/Models/ClientModel.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
 using Roster_Application.Models.Models_Interface;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Runtime.Serialization;
@@ -12,25 +13,31 @@ namespace Roster_Application.Models
         public int ClientId { get; set; }
 
         [Required]
+        [DisplayName ("Client Name:")]
         public string ?ClientName { get; set; }
 
         [Required]
+        [DisplayName ("Address:")]
         public string ?ClientAddress { get; set; }
 
         [Required]
+        [DisplayName ("Contact Number:")]
         public string ?ClientContactDetails { get; set; }
 
         [Required]
         [ForeignKey("CategoryId")]
+        [DisplayName ("Category:")]
         public int ?CategoryID {  get; set; }
         public CategoryModel ?CategoryId { get; set; }
 
         [Required]
         [ForeignKey("ScheduleId")]
+        [DisplayName("Schedule:")]
         public int ScheduleID { get; set; }
         public ScheduleModel ?ScheduleId { get; set; }
 
         [Required]
+        [DisplayName("Total Weekly Hours:")]
         public int TotalHours { get; set;}
    
     }
