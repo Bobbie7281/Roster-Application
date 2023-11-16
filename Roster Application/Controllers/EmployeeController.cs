86c8e4a85d4ec853adf80dbacec32e642b7a6608
Robert Ellul <rob.ellul@gmail.com>
2023-11-16 21:07:48 +0100

    Included a method to check the data before saving (still under progress)

--- Roster Application/Controllers/EmployeeController.cs
+++ Roster Application/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Roster_Application.Data;
 using Roster_Application.Models.Models_Interface;
+using System.Text.RegularExpressions;
 
 namespace Roster_Application.Controllers
 {
@@ -11,12 +12,15 @@ namespace Roster_Application.Controllers
         IClientModel? _clientModel;
         IEmployeeModel? _employeeModel;
         IScheduleModel? _scheduleModel;
-        public EmployeeController(ApplicationDbContext db, ICategoryModel categoryModel, IClientModel clientModel, IEmployeeModel employeeModel, IScheduleModel scheduleModel)
+        IListsModel? _listsModel;
+        public EmployeeController(ApplicationDbContext db, ICategoryModel categoryModel, IClientModel clientModel, 
+            IEmployeeModel employeeModel, IScheduleModel scheduleModel, IListsModel listsModel)
         {
             _db = db;
             _clientModel = clientModel;
             _employeeModel = employeeModel;
             _scheduleModel = scheduleModel;
+            _listsModel = listsModel;
             _categoryModel = categoryModel;
         }
         [HttpPost]
@@ -26,11 +30,23 @@ namespace Roster_Application.Controllers
         }
         public IActionResult CreateNewEmployee()
         {
-            return View();
+            _listsModel!.CategoryList=_db.Categories.ToList();
+            return View(_listsModel);
         }
         public IActionResult EditExistingEmployee()
         {
             return View();
+        }
+        [HttpPost]
+        public IActionResult SCheckDataPriorSaving(string ename, string esurname , string eaddress, string econtact, string eemailAddress, string ecategory) 
+        {
+            string error = "Orange";
+            string noError = "Green";
+
+            int errors = 0;
+
+            
+            throw new NotImplementedException();
         }
     }
 }
