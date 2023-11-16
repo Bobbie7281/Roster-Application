051104d646bdd8c51f165574613ef4f3996b2978
Robert Ellul <rob.ellul@gmail.com>
2023-11-16 21:06:05 +0100

    Edited the interface class for the Employee Model

--- Roster Application/Models/Models Interface/IEmployeeModel.cs
+++ Roster Application/Models/Models Interface/IEmployeeModel.cs
@@ -6,9 +6,11 @@ namespace Roster_Application.Models.Models_Interface
     {
         public int EmployeeId { get; set; }
         public string? EmployeeName { get; set; }
+        public string? EmployeeSurname {  get; set; }
         public int EmployeeAddress { get; set; }
         public int EmployeeContactNumber { get; set; }
         public int EmployeeEmail { get; set; }
-        public int EmployeeCategory { get; set; }
+        public int CategoryID { get; set; }
+        public CategoryModel? CategoryId { get; set; }
     }
 }
