f7593987b6f2234d5486201e72254a9fca700f6c
Robert Ellul <rob.ellul@gmail.com>
2023-11-16 21:10:17 +0100

    Added migration for the surname column

--- Roster Application/Migrations/20231115192303__AddedForeignKeyToEmployeeTable.Designer.cs
+++ Roster Application/Migrations/20231115192303__AddedForeignKeyToEmployeeTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Roster_Application.Data;
 
@@ -11,9 +12,11 @@ using Roster_Application.Data;
 namespace Roster_Application.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20231115192303__AddedForeignKeyToEmployeeTable")]
+    partial class _AddedForeignKeyToEmployeeTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -99,10 +102,6 @@ namespace Roster_Application.Migrations
                         .HasColumnType("int");
 
                     b.Property<string>("EmployeeName")
-                        .IsRequired()
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("EmployeeSurname")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
---
+++ Roster Application/Migrations/20231115192303__AddedForeignKeyToEmployeeTable.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Roster_Application.Migrations
+{
+    /// <inheritdoc />
+    public partial class _AddedForeignKeyToEmployeeTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "EmployeeCategory",
+                table: "Employees",
+                newName: "CategoryID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Employees_CategoryID",
+                table: "Employees",
+                column: "CategoryID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Employees_Categories_CategoryID",
+                table: "Employees",
+                column: "CategoryID",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Employees_Categories_CategoryID",
+                table: "Employees");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Employees_CategoryID",
+                table: "Employees");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryID",
+                table: "Employees",
+                newName: "EmployeeCategory");
+        }
+    }
+}
