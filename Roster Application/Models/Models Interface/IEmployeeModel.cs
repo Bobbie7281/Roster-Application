d809780a4ce2544085b02bd9e4e5cbb9311bc3cb
Robert Ellul <rob.ellul@gmail.com>
2023-11-17 23:51:17 +0100

    Updated the IEmployeeModel interface class to match the EmployeeModel class

--- Roster Application/Models/Models Interface/IEmployeeModel.cs
+++ Roster Application/Models/Models Interface/IEmployeeModel.cs
@@ -7,9 +7,9 @@ namespace Roster_Application.Models.Models_Interface
         public int EmployeeId { get; set; }
         public string? EmployeeName { get; set; }
         public string? EmployeeSurname {  get; set; }
-        public int EmployeeAddress { get; set; }
-        public int EmployeeContactNumber { get; set; }
-        public int EmployeeEmail { get; set; }
+        public string EmployeeAddress { get; set; }
+        public string EmployeeContactNumber { get; set; }
+        public string EmployeeEmail { get; set; }
         public int CategoryID { get; set; }
         public CategoryModel? CategoryId { get; set; }
     }
