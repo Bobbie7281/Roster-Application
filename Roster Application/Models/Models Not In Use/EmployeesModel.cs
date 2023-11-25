95399a8d5c70920cd2a54dd6219ed5cec1cd1138
Robert Ellul <rob.ellul@gmail.com>
2023-11-25 23:27:02 +0100

    Moved the old Employees Model to another folder as it is not being used

---
+++ Roster Application/Models/Models Not In Use/EmployeesModel.cs
@@ -0,0 +1,36 @@
+﻿using Roster_Application.Models.Models_Interface;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Roster_Application.Models
+{
+    public class EmployeesModel : IEmployeeModel
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        [Required]
+        [DisplayName("Employee Name:")]
+        public string? EmployeeName { get; set; }
+        [Required]
+        [DisplayName("Surname:")]
+        public string? EmployeeSurname { get; set; }
+        [Required]
+        [DisplayName("Employee Address:")]
+        public string? EmployeeAddress { get; set; }
+        [Required]
+        [DisplayName("Employee Contact Number:")]
+
+        public string? EmployeeContactNumber { get; set; }
+        [Required]
+        [DisplayName("Employee Email Address:")]
+
+        public string? EmployeeEmail { get; set; }
+
+        [Required]
+        [ForeignKey("CategoryId")]
+        [DisplayName("Category")]
+        public int CategoryID { get; set; }
+        public CategoryModel? CategoryId { get; set; }
+    }
+}
