0b49615dd85d28ff5268dbb832bc4764f2714425
Robert Ellul <rob.ellul@gmail.com>
2023-11-25 23:29:09 +0100

    Created a new dbset for the new Employee Model

--- Roster Application/Data/ApplicationDbContext.cs
+++ Roster Application/Data/ApplicationDbContext.cs
@@ -9,9 +9,10 @@ namespace Roster_Application.Data
 
         public DbSet<ClientModel>Clients { get; set; }
         public DbSet<ScheduleModel> Schedules { get; set; }
-        public DbSet<EmployeeModel> Employees { get; set; }
         public DbSet<CategoryModel> Categories { get; set; }
-       
+        public DbSet<EmployeesModel> Employee { get; set; }
+        public DbSet<EmployeesModel> Employees { get; set; }//not in use
+
     }
   
 }
